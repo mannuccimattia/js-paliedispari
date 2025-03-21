8bec60b9310b3f44f70a1ba946bb75730b66cd57
mannuccimattia <mannuccimattia@gmail.com>
2025-03-21 15:23:13 +0100

    [UPD] Done checkPalindrome

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 /* ******************************************** */
 /*                  PALINDROMA                  */
 /* ******************************************** */
+
 // definisco la funzione per capire se la parola è palindroma
 function checkPalindrome(word){
-  // creo un array vuoto per la parola inserita dall'utente (word)
+  // creo un array vuoto per la parola inserita dall'utente
   const wordArray = [];
   // ciclo word per ogni lettera inserita
   for(i=0; i<word.length; i++){
     //inserisco ogni lettera di word dentro wordArray
     wordArray.push(word[i]);
-    // stampa di controllo per ogni lettera
-    console.log(word[i]);
   }
   // stampa di controllo per wordArray
   console.log("Parola inserita:", wordArray);
@@ -19,7 +18,20 @@ function checkPalindrome(word){
   const wordReversedArray = wordArray.toReversed();
   // stampa di controllo per wordReversedArray
   console.log("Il contrario di", word, "è", wordReversedArray);
-}
+
+  // creo una variabile di controllo
+  let isPalindrome = false;
+  // controllo se la parola è uguale al suo contrario e stampo il risultato
+  if(wordArray.toString() == wordReversedArray.toString() && isPalindrome == false){
+    isPalindrome = true;
+    console.log("La parola", word, "è palindroma.");
+  }
+  if(wordArray != wordReversedArray && isPalindrome == false){
+    console.log("La parola", word, "non è palindroma");
+  }
+} 
+// fine funzione
+
 
 // definisco la variabile userWord e le assegno un valore immesso dall'utente
 let userWord = prompt("Inserisci la parola da controllare.");
